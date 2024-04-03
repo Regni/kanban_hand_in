154fdb3d14f09918254970b1e8cb03fde99acbc8
Alexander Kuiper <kuiper9000@gmail.com>
2024-04-03 17:22:30 +0200

    added the local storage to save the data

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,12 +1,19 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import DataContext from "./context/DataContext";
 const Post = ({ title, content, id, date }) => {
+  const { posts, setPosts } = useContext(DataContext);
+
+  const handleDelete = (id) => {
+    const newPostsList = posts.filter((task) => task.id != id);
+    setPosts(newPostsList);
+  };
+
   return (
     <div className="Card" draggable id={id}>
       <h3>{title}</h3>
       <p>{date}</p>
       <p>{content}</p>
-      <button>delete card</button>
+      <button onClick={() => handleDelete(id)}>delete card</button>
     </div>
   );
 };
--- src/context/DataContext.jsx
+++ src/context/DataContext.jsx
@@ -5,17 +5,16 @@ const DataContext = createContext({});
 
 export const DataProvider = ({ children }) => {
   //useState that will hold each task/post
-  const [posts, setPosts] = useState([
-    {
-      id: 2,
-      title: "First Post",
-      content: "This is the post content",
-      date: new Date().toDateString(),
-      assignedTo: 0,
-    },
-  ]);
+  const [posts, setPosts] = useState(
+    JSON.parse(localStorage.getItem("allTasks"))
+  );
   //useState that will contain a array of names of columns their index is also in which order they will show up
   const [columns, setColumns] = useState(["doing", "done"]);
+
+  useEffect(() => {
+    localStorage.setItem("allTasks", JSON.stringify(posts));
+    console.log("running effect");
+  }, [posts]);
 
   return (
     <DataContext.Provider value={{ posts, setPosts, columns }}>
